44691f13285673f761b4313d82532a0dff782925
Chris Becco <crbp8b@mail.umkc.edu>
2018-07-23 13:19:20 -0500

    Update index.php

--- Intern Project/index.php
+++ Intern Project/index.php
@@ -10,8 +10,8 @@
   <!-- =============== Nav Bar of website ==================== -->
 <ul>
   <img src="Logo.png" alt="YRC_Logo" style="height:inherit;float:left;">
-  <li><a href="index.php" href="contact_form.html">Home</a></li>
-  <li><a href="#contact">Contact</a></li>
+  <li><a href="index.php" >Home</a></li>
+  <li><a href="contact_form.html">Contact</a></li>
   <li><a href="#about">About</a></li>
 </ul>
 
